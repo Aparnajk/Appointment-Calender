9a90e803892bed8f2413c171fbe74f5015589a96
Aparnajk <63504484+Aparnajk@users.noreply.github.com>
2020-04-11 19:34:32 +0530

    Add files via upload

---
+++ CalenderContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebProject.Models
+{
+    public class CalendarContext : DbContext
+    {
+        public CalendarContext() : base()
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<CalendarContext>());
+        }
+        public System.Data.Entity.DbSet<WebProject.Models.Appointment> Appointments { get; set; }
+    }
+}
